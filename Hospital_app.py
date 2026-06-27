53fb520e2672c29bd009fec507f38a456f68caa4
marvin34261-collab <marvin34261@gmail.com>
2026-06-27 12:00:22 +0700

    Update Hospital_app.py

--- Hospital_app.py
+++ Hospital_app.py
@@ -6,7 +6,7 @@ import os
 
 st.set_page_config(page_title="Smart Hospital Patient Navigator", page_icon="🏥", layout="wide")
 
-sst.markdown("""
+st.markdown("""
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Inter: wght@400;500;600;700&display=swap');
 
