fecbe7ec1a418daca7e98f79c68d683262bd1c4c
marvin34261-collab <marvin34261@gmail.com>
2026-06-27 10:42:55 +0700

    Add caching for model loading in Hospital_app

--- Hospital_app.py
+++ Hospital_app.py
@@ -33,3 +33,8 @@ font-size:14px !important; font-weight: 500 !important; color: #3374151 !importa
 }
 </style>
 """,unsafe_allow_html=True)
+
+@st.cache_resource
+def load__model():
+  with open('hostpital_model.pk1', 'rb') as f:
+    return pickle.load(f)
